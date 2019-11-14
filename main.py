41df648fe18225d8e45c2f1d696c29693ed6cd0a
TiMuR228228 <57760359+TiMuR228228@users.noreply.github.com>
2019-11-14 16:58:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import sys
+from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QApplication, QMainWidow, QPushButton
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5 import Qt
+import random, copy
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.title = "PyQt5 круги"
+        self.draw = None
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.button1 = QPushButton(self)
+        self.button1.clicked.connect(self.run)
+        self.show()
+
+    def run(self):
+        self.draw = True
+        self.update()
+
+    def painEvent(self, event):
+        if self.draw:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(QPen(Qt.yellow, 3, Qt.SolidLine))
+            rad = random.randrange(255)
+            x = random.rangrenge(255)
+            y = random.rangrenge(255)
+            qp.drawEllipse(x, y, rad, rad)
+            qp.end()
+
+
+App = QApplication(sys.argv)
+window = Window()
+sys.exit(App.exec())
